4f33b0bc5615a52fdfbb05121cd2e756ea332cf1
Nafiur Rahman <nafiurrahman03112001@gmail.com>
2023-06-18 20:43:10 +0600

    Add files via upload

---
+++ ConsoleArray/ConsoleArray/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1D array________
+
+            int[] a = new int[3] { 1, 2, 3 };
+
+
+
+            /*byte x = 0;
+            while (x < a.Length)
+            {
+                Console.WriteLine("{0} ", a[x]);    // show with while loop
+                x++;
+            }*/
+
+
+
+            /* for(int i = 0; i <a.Length; i++)
+             {
+                 Console.WriteLine("{0}",a[i]);     // with for loop
+             }*/
+
+
+
+            /* byte c = 0;
+            do
+            {
+                Console.WriteLine(a[c]);            //with do while loop
+                c++;
+            }
+            while (c < a.Length);*/
+           
+            foreach (int i in a)
+            {
+                Console.WriteLine("{0}", i);
+            }
+
+
+
+            //2D or multi D array___________ 
+
+            int[,] m = new int[3,3] { { 1 ,2, 3 }, { 4,5,6}, { 7,8,9 } };
+
+/*
+            byte r = 0;
+            while (r < m.Length)
+            {
+                byte c = 0;
+
+                while (c < 3)
+                {
+                    Console.Write("{0} ", m[r, c]);       //to show in matrix form .Write()
+                    c++;
+
+                }
+                Console.WriteLine();
+                r++;
+
+            }*/
+
+
+                        //**************JAGGED ARRAY **************
+
+            int[][] ja= new int[4][];       //only provide row value
+
+ /*           ja[0] = new int[2] { 1, 2 };
+            ja[1] = new int[4] { 5, 2,6,7 };        //array in array 
+            ja[2] = new int[3] { 4, 5, 3 };
+            ja[3] = new int[4] {11,2,3,4 };
+
+            byte r = 0; 
+            while(r<ja.Length)
+            {
+                byte c = 0;
+                while (c < ja[r].Length)            //ja[r].length
+                {
+                    Console.Write("{0} ",ja[r][c]);
+                    c++;
+                }
+                Console.WriteLine();
+                r++;
+            }*/
+
+
+
+        }
+    }
+}
---
+++ Structure/Structure/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+
+    struct OurPoint
+    {
+        public int x, y;     //user define variable (member variable)
+
+       /* public OurPoint()
+        {
+            x = 0;                //default constructor 
+            y = 0;              //can't decleared by programmer cause it is buit in 
+        }
+*/
+        public OurPoint(int x, int y)
+        {
+            this.x = x;             //peramitarized constructor (local variable)
+            this.y = y;             //we use (this) key word to avoid AMBIGUITY
+                                    //***((cause there multiple content with same name))***
+
+        }
+        
+        //There is no static variables 
+
+        public void show()
+        {
+            Console.WriteLine("({0},{1})",this.x,this.y);       //method
+        }
+
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            OurPoint p = new OurPoint();
+            p.x = 10;
+            p.y = 20;  
+            p.show();
+
+
+            OurPoint p1;    //Fixed memory Allocation 
+
+            Console.Write("Enter the point values: ");
+            p1.x = Convert.ToInt32(Console.ReadLine());
+            p1.y = Convert.ToInt32(Console.ReadLine());
+            p1.show();
+           
+
+            Console.Write("pass value through constructor: ");
+            //user input through constructor 
+            OurPoint p2 = new OurPoint(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter the point values: ");
+            p2.show();
+
+           
+            Console.Write("\nDirect value thrugh constructor: ");
+
+            OurPoint p3 = new OurPoint(3,4);   //dynamically memory allocation bacause of new key word
+            p3.show();
+           
+        }
+    }
+}
